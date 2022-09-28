8c03aa5dbc7818b7bc8726a8c771f2f1c92ad67b
KeerthiNarayana <keerthinarayanaprasad2009@gmail.com>
2022-09-28 07:59:00 +0530

    Updates

---
+++ precache-manifest.44ddee6d6f77eb45a4532f739b612ac4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2cdd7cecff5a8e8c10725e3a742d653c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "7fbed4848b26d195024a",
+    "url": "./static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "78cbe2acb5cc10f7b01c",
+    "url": "./static/js/2.918821f2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.918821f2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7fbed4848b26d195024a",
+    "url": "./static/js/main.d064b67b.chunk.js"
+  },
+  {
+    "revision": "df41fa2c06feddb76aed",
+    "url": "./static/js/runtime-main.10d47835.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./static/media/logo.5d5d9eef.svg"
+  }
+]);
