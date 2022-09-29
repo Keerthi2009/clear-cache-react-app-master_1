a3bfb4e1f560ba37bc50307c320adf416d341856
KeerthiNarayana <keerthinarayanaprasad2009@gmail.com>
2022-09-29 08:50:08 +0530

    newco

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function MainApp(props) {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Build date: {getBuildDate(packageJson.buildDate)} changes</p>
+        <p>Build date: {getBuildDate(packageJson.buildDate)} cha</p>
       </header>
     </div>
   );
